7fd5efa9971e4682715fa87cdc4c2bc373060b55
zehra abasiyun <zehraabasiyun@gmail.com>
2024-11-06 22:33:12 +0300

    Proje dosyası ekle.

---
+++ REST-API/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace REST_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hello world");
+        }
+    }
+}
