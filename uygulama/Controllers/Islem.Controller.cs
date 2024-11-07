16167fb6beedd7e98cc3233c1235911084731215
zehra abasiyun <zehraabasiyun@gmail.com>
2024-11-07 14:57:41 +0300

    İki sayıyı topla eklendi

---
+++ uygulama/Controllers/Islem.Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace uygulama.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IslemController : ControllerBase
+    {
+        [HttpGet("add")]
+        public ActionResult<int> AddTwoNumbers([FromQuery] int sayi1, [FromQuery] int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            return Ok(sonuc);
+        }
+    }
+}
+
