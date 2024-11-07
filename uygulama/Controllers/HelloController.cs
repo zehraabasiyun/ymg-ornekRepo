4bf56ffa61985531c09ecea5673c2dd5fb418ce0
zehra abasiyun <zehraabasiyun@gmail.com>
2024-11-07 14:53:18 +0300

    REST-API uygulaması eklendi

---
+++ uygulama/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace uygulama.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hello world");
+        }
+    }
+}
