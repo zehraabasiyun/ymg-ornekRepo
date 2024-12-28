e6c9b0c53a3a035888942a4a28b7dd2c313d12aa
Zehra Abasıyün <zehraabasiyun@gmail.com>
2024-12-29 00:36:14 +0300

    my-rest-api klasörü eklendi

---
+++ my-rest-api/REST-API/Controllers/IslemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using REST_API.Models;
+
+namespace REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IslemController : ControllerBase
+    {
+        [HttpPost("carpim")]
+        public IActionResult PostCarpim([FromBody] CarpimRequest request)
+        {
+            int carpim = request.Sayı1 * request.Sayı2;
+
+            return Ok(new { sonuc = carpim });
+        }
+
+        [HttpGet("topla")]
+        public IActionResult GetTopla([FromQuery] int Sayı1, [FromQuery] int Sayı2)
+        {
+            int toplam = Sayı1 + Sayı2;
+
+            return Ok(new { sonuc = toplam });
+        }
+    }
+}
---
+++ my-rest-api/REST-API/Models/CarpimRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace REST_API.Models
+{
+    public class CarpimRequest
+    {
+        public int Sayı1 { get; set; }
+        public int Sayı2 { get; set; }
+    }
+}
